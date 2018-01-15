173eb89d3cb147d19fa4244c02a33f9cd4e484ad
Duong Nguyen Anh Khoa <dnanhkhoa@live.com>
2018-01-15 23:01:28 +0700

    Updated

--- spider.py
+++ spider.py
@@ -16,6 +16,9 @@ DATABASE_FILE = 'songs.db'
 
 PLAYLIST_PATTERN = regex.compile(r'title:"([^"]+)"[^=]+=(\d+)[^(]+\("([^"]+)"\)', regex.IGNORECASE)
 
+LOGGER = logone.get_logger(__name__)
+LOGGER.set_level(level=logone.INFO)
+
 sched = BlockingScheduler()
 
 """
@@ -47,7 +50,7 @@ def get_playlist_dict(timeout=15):
                     'url': item[2]
                 }
     except Exception as e:
-        debug(e)
+        LOGGER.exception(e)
     return playlist_dict
 
 
@@ -66,7 +69,7 @@ def get_distinct_song_ids(old_playlist_dict, new_playlist_dict):
 @sched.scheduled_job('cron', hour=22)
 def sync():
     try:
-        debug('Running:', str(datetime.now()))
+        LOGGER.info('Running: ' + str(datetime.now()))
 
         # Check authentication credentials
         DROPBOX.users_get_current_account()
@@ -78,43 +81,45 @@ def sync():
             old_playlist_dict = {}
 
         # Get new playlist
-        debug('Fetching new songs...')
+        LOGGER.info('Fetching new songs...')
         new_playlist_dict = get_playlist_dict()
-        debug('Number of songs: %d' % len(new_playlist_dict))
+        LOGGER.info('Number of songs: %d' % len(new_playlist_dict))
 
         # Get distinct song indices
         distinct_song_ids = get_distinct_song_ids(old_playlist_dict, new_playlist_dict)
-        debug('%d new songs' % len(distinct_song_ids))
+        LOGGER.info('%d new songs' % len(distinct_song_ids))
 
         songs_db = {}
         if distinct_song_ids:
             # Upload all songs to the new folder
             for i in distinct_song_ids:
-                debug('Processing file: %s - %s' % (i, new_playlist_dict[i]['name']))
+                LOGGER.info('Processing file: %s - %s' % (i, new_playlist_dict[i]['name']))
                 try:
-                    debug('--Downloading file')
+                    LOGGER.info('--Downloading file')
                     song_data = download_file(url=PAGE_URL + '/' + new_playlist_dict[i]['url'])
                     if song_data:
                         file_name = DROPBOX_BASE_PATH + '/%s - %s.mp3' % (i, new_playlist_dict[i]['name'])
 
-                        debug('--Uploading file')
+                        LOGGER.info('--Uploading file')
                         DROPBOX.files_upload(song_data, file_name, WriteMode.overwrite, mute=True)
 
                         songs_db[i] = {
                             'name': new_playlist_dict[i]['name'],
                             'file': file_name
                         }
-                        debug('--OK')
+                        LOGGER.info('--OK')
                     else:
-                        debug('--Download failed:', PAGE_URL + '/' + new_playlist_dict[i]['url'])
+                        LOGGER.info('--Download failed: ' + PAGE_URL + '/' + new_playlist_dict[i]['url'])
                 except Exception as err:
-                    debug('--Upload failed:', err)
+                    LOGGER.info('--Upload failed')
+                    LOGGER.exception(err)
                     if isinstance(err, ApiError) and err.error.is_path() and \
                             err.error.get_path().reason.is_insufficient_space():
                         break
                     continue
 
             full_songs_db = {**old_playlist_dict, **songs_db}
+
             # Save database file
             write_json(full_songs_db, DATABASE_FILE)
 
@@ -124,15 +129,15 @@ def sync():
                     json.dumps(full_songs_db, indent=4, ensure_ascii=False).encode('UTF-8'), DROPBOX_DB_PATH,
                     WriteMode.overwrite, mute=True)
             except ApiError as apierr:
-                debug(apierr)
+                LOGGER.exception(apierr)
 
-        debug('Done: %d songs' % len(songs_db))
+        LOGGER.info('Done: %d songs' % len(songs_db))
     except Exception as e:
-        debug(e)
+        LOGGER.exception(e)
 
 
 def main():
-    debug('Scheduling:', str(datetime.now()))
+    LOGGER.info('Scheduling: ' + str(datetime.now()))
     sched.start()
 
 
